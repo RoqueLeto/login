8fa21d9afd3ba4807b1aa63792f3108d19b79314
RoqueLeto <roqueleto@gmail.com>
2023-09-04 15:48:58 -0300

    Insert com postman

--- server/server.js
+++ server/server.js
@@ -1,30 +1,42 @@
 const express = require("express");
 const cors = require("cors");
-const pool = require("./database");
+const { Pool } = require("pg");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/adduser", (req, res) => {
-  const username = req.body["username"]
-  const password = req.body["password"]
-
-  console.log("Username: " + username);
-  console.log("Password: " + password);
-
-  const insertSTMT = `INSERT INTO accounts ( username, password ) VALUES ('${username}', '${password} ); `;
-
-  pool.query(insertSTMT).then((response) => {
-    console.log('Aqui', response);
-  })
-    .catch((err) => { 
-      console.log(err);
-    });
-
-  console.log(req.body);
-  res.send("Response Received: " + req.body);  
+const pool = new Pool({
+  user: "postgres",
+  password: "crtb@300419!",
+  host: "localhost",
+  port: 5432,
+  database: "db_login_system", // Verifique o nome do seu banco de dados
 });
- 
-app.listen(4000, () => console.log("Sever on localhost:4000"));
+
+app.post("/adduser", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Verifica se o nome de usuário já existe na tabela 'accounts'
+    const checkUserQuery = "SELECT username FROM accounts WHERE username = $1";
+    const existingUser = await pool.query(checkUserQuery, [username]);
+
+    if (existingUser.rows.length > 0) {
+      // Nome de usuário já existe, retorne uma resposta apropriada
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // Se o nome de usuário não existe, insira o novo registro
+    const insertQuery = "INSERT INTO accounts (username, password) VALUES ($1, $2)";
+    await pool.query(insertQuery, [username, password]);
+
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(4000, () => console.log("Server on localhost:4000"));
