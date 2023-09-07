3b82b6f391a5c91d97875d15cb6a00a1df1b3d39
RoqueLeto <roqueleto@gmail.com>
2023-09-07 20:04:44 -0300

    003

--- server/database.js
+++ server/database.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const mainPool = new Pool({
   user: "postgres",
   password: "crtb@300419!",
   host: "localhost",
   port: 5432,
-  database: "db_login_system",
+  database: "postgres", // Use o banco de dados "postgres" para criar o novo banco
 });
 
 const createDbQry = `CREATE DATABASE db_login_system`;
@@ -16,24 +16,20 @@ const createTblQry = `CREATE TABLE IF NOT EXISTS accounts (
 
 (async () => {
   try {
+    // Cria o banco de dados "db_login_system" se ele não existir
+    await mainPool.query(createDbQry);
+    console.log("Database created successfully.");
 
-    // Verifica a existência do banco de dados
-    const checkDbExistsQry = `SELECT 1 FROM pg_database WHERE datname = 'db_login_system'`;
-    const result = await pool.query(checkDbExistsQry);
-    const dbExists = result.rowCount > 0;
+    // Agora, você pode criar um novo pool para se conectar ao banco de dados "db_login_system"
+    const pool = new Pool({
+      user: "postgres",
+      password: "crtb@300419!",
+      host: "localhost",
+      port: 5432,
+      database: "db_login_system",
+    });
 
-    if (!dbExists) {
-      // Cria o banco de dados "db_login_system"
-      await pool.query(createDbQry);
-      console.log("Database created successfully.");
-    } else {
-      console.log("Database already existsBanco já Existe.");
-    }
-
-    // Conecta-se ao banco de dados "db_login_system"
-    pool.database = "db_login_system";
-
-    // Cria a tabela "accounts"
+    // Cria a tabela "accounts" se ela não existir
     await pool.query(createTblQry);
 
     console.log("Tabela Criada com Sucesso.");
