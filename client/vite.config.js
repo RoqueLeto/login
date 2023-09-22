68f327142737aedaf1211248c7d59f23658702a7
RoqueLeto <roqueleto@gmail.com>
2023-09-21 21:12:41 -0300

    att.. Main

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [react()],
-})
-module.exports = {
   build: {
     outDir: './dist',
     rollupOptions: {
       external: ['vue'],
     },
   },
-};
+});
+
+export default config;
